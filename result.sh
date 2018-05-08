1166767930f77a6166aac5852c60bd7d74fd0fa5
Valentine Radulevich <valentine@ex.ua>
2018-05-08 21:19:05 +0300

    Delete |

--- result.sh
+++ result.sh
@@ -11,13 +11,13 @@ find ${KERNEL_PATH} -name "dt.img";
 find ${KERNEL_PATH} -name "*.ko";
 
 curl --upload-file ./dt.img https://transfer.sh/dt.img
-echo "|"
+echo " "
 curl --upload-file ./arch/arm/boot/zImage https://transfer.sh/zImage
-echo "|"
+echo " "
 curl --upload-file ./drivers/mmc/card/mmc_test.ko https://transfer.sh/mmc_test.ko
-echo "|"
+echo " "
 curl --upload-file ./drivers/autotst/autotst.ko https://transfer.sh/autotst.ko
-echo "|"
+echo " "
 
 echo So you now know where everything is and leaked LOLOLOLOLOLOL;
 
